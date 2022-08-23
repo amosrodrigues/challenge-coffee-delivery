dc56ee110e4d3f80cea3ef64aa5032a8fdd7712e
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-08-23 20:23:35 -0300

    feat: implement form checkout.

--- src/pages/Checkout/CheckoutForm/index.tsx
+++ src/pages/Checkout/CheckoutForm/index.tsx
@@ -1,11 +1,22 @@
-import { MapPinLine } from 'phosphor-react';
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from 'phosphor-react';
 import {
   Address,
   AddressHeader,
   FormContainer,
   Payment,
-  AddressLocation,
   AddressFields,
+  AddressInpunt,
+  AddressWithTwoFields,
+  AddressWithThreeFields,
+  PaymentHeader,
+  PaymentOptions,
+  PaymentOptionsCard,
 } from './styles';
 
 export function CheckoutForm() {
@@ -14,16 +25,54 @@ export function CheckoutForm() {
       <Address>
         <AddressHeader>
           <MapPinLine size={22} />
-          <AddressLocation>
+          <div>
             <h3>Endereço de Entrega</h3>
             <p>Informe o endereço onde deseja receber seu pedido</p>
-          </AddressLocation>
+          </div>
         </AddressHeader>
 
-        <AddressFields></AddressFields>
+        <AddressFields>
+          <AddressInpunt placeholder="CEP" />
+          <AddressInpunt placeholder="Rua" />
+          <AddressWithTwoFields>
+            <AddressInpunt placeholder="Número" />
+            <AddressInpunt placeholder="Complemento" />
+            <span>Opcional</span>
+          </AddressWithTwoFields>
+          <AddressWithThreeFields>
+            <AddressInpunt placeholder="Bairro" />
+            <AddressInpunt placeholder="Cidade" />
+            <AddressInpunt placeholder="UF" />
+          </AddressWithThreeFields>
+        </AddressFields>
       </Address>
 
-      <Payment></Payment>
+      <Payment>
+        <PaymentHeader>
+          <CurrencyDollar size={22} />
+          <div>
+            <h3>Pagamento</h3>
+            <p>
+              O pagamento é feito na entrega. Escolha a forma que deseja pagar
+            </p>
+          </div>
+        </PaymentHeader>
+
+        <PaymentOptions>
+          <PaymentOptionsCard>
+            <CreditCard size={16} />
+            <p>CARTÃO DE CRÉDITO</p>
+          </PaymentOptionsCard>
+          <PaymentOptionsCard>
+            <Bank size={16} />
+            <p>CARTÃO DE DÉBITO</p>
+          </PaymentOptionsCard>
+          <PaymentOptionsCard selected>
+            <Money size={16} />
+            <p>DINHEIRO</p>
+          </PaymentOptionsCard>
+        </PaymentOptions>
+      </Payment>
     </FormContainer>
   );
 }
--- src/pages/Checkout/CheckoutForm/styles.ts
+++ src/pages/Checkout/CheckoutForm/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const FormContainer = styled.div`
   display: flex;
@@ -21,32 +21,153 @@ export const Address = styled.div`
   background: ${(props) => props.theme['base-card']};
 `;
 
-export const AddressHeader = styled.div`
+const BaseHeader = styled.div`
   display: flex;
   gap: 0.5rem;
 
-  svg {
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 0.125rem;
+
+    h3 {
+      color: ${(props) => props.theme['base-subtitle']};
+      font: 400 1rem 'Roboto';
+      line-height: 21px;
+    }
+
+    p {
+      font: 400 0.875rem 'Roboto';
+      line-height: 18px;
+    }
+  }
+`;
+
+export const AddressHeader = styled(BaseHeader)`
+  > svg {
     color: ${(props) => props.theme['yellow-dark']};
   }
 `;
 
-export const AddressLocation = styled.div`
+export const AddressFields = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.125rem;
+  gap: 1rem;
+`;
 
-  h3 {
-    color: ${(props) => props.theme['base-subtitle']};
-    font: 400 1rem 'Roboto';
-  }
+export const AddressInpunt = styled.input`
+  background: ${(props) => props.theme['base-input']};
+  color: ${(props) => props.theme['base-label']};
+  border: 1px solid ${(props) => props.theme['base-button']};
+  border-radius: 4px;
 
-  p {
-    font: 400 0.875rem 'Roboto';
+  font-size: 0.875rem;
+  padding: 0.75rem;
+  height: 2.625rem;
+
+  :nth-child(1) {
+    width: 12.5rem;
   }
 `;
 
-export const AddressFields = styled.div``;
+export const AddressWithTwoFields = styled.div`
+  display: flex;
+  gap: 0.75rem;
+  position: relative;
+
+  input:nth-child(1) {
+    width: 12.5rem;
+  }
+
+  input:nth-child(2) {
+    flex: 1;
+    padding-right: 4.5rem;
+  }
+
+  span {
+    position: absolute;
+    right: 0;
+    display: flex;
+    align-items: center;
+    font-size: 0.875rem;
+    font-style: italic;
+    padding: 0.75rem;
+    color: ${(props) => props.theme['base-label']};
+  }
+`;
+
+export const AddressWithThreeFields = styled.div`
+  display: flex;
+  gap: 0.75rem;
+
+  input:nth-child(1) {
+    width: 12.5rem;
+  }
+
+  input:nth-child(1) {
+    flex: 1;
+  }
+
+  input:nth-child(3) {
+    width: 3.75rem;
+  }
+`;
 
 export const Payment = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  gap: 2rem;
+
+  border-radius: 6px;
   background: ${(props) => props.theme['base-card']};
+`;
+
+export const PaymentHeader = styled(BaseHeader)`
+  > svg {
+    color: ${(props) => props.theme.purple};
+  }
+`;
+
+export const PaymentOptions = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-gap: 0.75rem;
+`;
+
+interface PaymentOptionsCardProps {
+  selected?: boolean;
+}
+
+export const PaymentOptionsCard = styled.div<PaymentOptionsCardProps>`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  gap: 0.75rem;
+
+  background: ${(props) => props.theme['base-button']};
+  color: ${(props) => props.theme['base-text']};
+  border-radius: 6px;
+  font-size: 0.75rem;
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) => props.theme['base-hover']};
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+
+  ${(props) =>
+    props.selected &&
+    css`
+      background: ${(props) => props.theme['purple-light']};
+      border: 1px solid ${(props) => props.theme.purple};
+    `}
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
 `;

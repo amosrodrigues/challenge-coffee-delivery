223c67ebb3bb67647ecfd2c194f88e467be19c44
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-08-26 00:11:26 -0300

    feat: implement error view.

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -4,14 +4,12 @@ import {
   InputHTMLAttributes,
 } from 'react';
 
-import { FieldError } from 'react-hook-form';
 import { InpuntStyled } from './styles';
 
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name?: string;
   label?: string;
   error?: boolean;
-  placeholder?: string;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
@@ -22,7 +20,6 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
     <>
       {!!label && <label htmlFor={name}>{label}</label>}
       <InpuntStyled id={name} name={name} error={error} {...props} ref={ref} />
-      {/* {!!error && <p>{error.message}</p>} */}
     </>
   );
 };
--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 interface InpuntStyled {
-  error: boolean;
+  error?: boolean;
 }
 
 export const InpuntStyled = styled.input<InpuntStyled>`
